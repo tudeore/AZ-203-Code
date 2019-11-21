be42b36ddaf435528a9cff5d5f1b63d355c35127
tushar deore <deoretushar10@gmail.com>
2019-11-21 19:03:21 +0000

    Updating Read function

--- AzureSqlDatabase/Program.cs
+++ AzureSqlDatabase/Program.cs
@@ -16,7 +16,44 @@ namespace AzureSqlDatabase
 
         static void Main(string[] args)
         {
-            CreateContainer("wizhlabsdemonew").GetAwaiter().GetResult();
+            //CreateContainer("wizhlabsdemonew").GetAwaiter().GetResult();
+            //UploadBlob("wizhlabsdemonew", "Sample.txt", "D://Sample.txt").GetAwaiter().GetResult();
+            GetBlobProperties("wizhlabsdemonew", "Sample.txt").GetAwaiter().GetResult();
+        }
+
+        static async Task GetBlobProperties(string containerName, string blobName)
+        {
+            CloudStorageAccount wizhlabs_storage = CloudStorageAccount.Parse(conn_string);
+            CloudBlobClient wizhlabs_blobClient = wizhlabs_storage.CreateCloudBlobClient();
+            CloudBlobContainer container = wizhlabs_blobClient.GetContainerReference(containerName);
+
+            BlobContinuationToken blobContinuationToken = null;
+            do
+            {
+                var results = await container.ListBlobsSegmentedAsync(null, blobContinuationToken);
+                blobContinuationToken = results.ContinuationToken;
+                foreach (CloudBlockBlob item in results.Results)
+                {
+                    Console.WriteLine(item.Uri);
+                    Console.WriteLine(item.Properties.Created);
+                    Console.WriteLine(item.Properties.ETag);
+                }
+            } while (blobContinuationToken != null);
+
+            Console.ReadKey();
+        }
+
+        static async Task UploadBlob(string p_containerName, string p_fileName, string p_path)
+        {
+            CloudStorageAccount wizhlabs_storage = CloudStorageAccount.Parse(conn_string);
+            CloudBlobClient wizhlabs_blobClient = wizhlabs_storage.CreateCloudBlobClient();
+            CloudBlobContainer container = wizhlabs_blobClient.GetContainerReference(p_containerName);
+
+            CloudBlockBlob cloudBlockBlob = container.GetBlockBlobReference(p_fileName);
+            await cloudBlockBlob.UploadFromFileAsync(p_path);
+
+            Console.WriteLine("Object uploaded");
+            Console.ReadKey();
         }
 
         static async Task CreateContainer(string p_Name)
